cdb83c7629c74d2c294e4f07135adc855d04c6dd
hasangkz <hassan.gokgoz@gmail.com>
2023-12-02 01:04:45 +0300

    gitignore bug fix

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import basketSlice from './features/BasketSlice';
-export const store = configureStore({
-  reducer: {
-    basket: basketSlice,
-  },
-});
